6bab57475b01984b85d1e235ff590f234d676a26
dyf189 <100421701+dyf189@users.noreply.github.com>
2026-06-29 19:53:03 +0800

    更新 toolsinstall.sh
    使用shell脚本配置uv

--- toolsinstall.sh
+++ toolsinstall.sh
@@ -1,5 +1,5 @@
 echo "本程序将在当前目录安装并配置USFM所需要的环境和工具"
-echo "正在配置python虚拟环境，这将会安装python3.10-venv"
+echo "正在配置python虚拟环境，将在系统中安装uv包管理器"
 
 read -p "输入继续(Y/n):" data
 if [[ $data == "n" || $data == "N" ]]; then
@@ -7,9 +7,17 @@ if [[ $data == "n" || $data == "N" ]]; then
     exit 0
 fi
 
-sudo apt install python3.10-venv
-python3 -m venv .venv
-source .venv/bin/activate
+read -p "选择安装方式curl(输入c)或者wget(输入w):" data
+if [[ $data == "c"]];
+then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+else
+    wget -qO- https://astral.sh/uv/install.sh | sh
+fi
 
-echo "正在下载工具minecraft-nbt-editor"
-pip install click>=8.0.0 colorama>=0.4.4 rich>=12.0.0
+uv venv
+source .venv/bin/activate
+uv pip install json5
+
+#echo "正在下载工具minecraft-nbt-editor"
+#pip install click>=8.0.0 colorama>=0.4.4 rich>=12.0.0
